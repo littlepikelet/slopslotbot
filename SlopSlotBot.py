ab41fd75e6bf637008b044df9a4701fcfe0c4149
littlepikelet <littlepikelet@gmail.com>
2026-05-09 01:28:23 +0500

    Fix DiceEmoji import

--- SlopSlotBot.py
+++ SlopSlotBot.py
@@ -303,6 +303,7 @@ async def remove_manual_slot(message: types.Message):
 async def main():
     await init_db()
     print("Бот запущен")
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot, allowed_updates=["message", "callback_query"])
 
 if __name__ == "__main__":
